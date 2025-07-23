9f40f3432e182c38db54ae0c8157d018eeef6c32
Nihantra Patel <nihantra@frappe.io>
2025-07-23 12:32:39 +0530

    fix: update patch for rename_is_accrued_to_demand_generated

--- lending/patches/v1_0/rename_is_accrued_to_demand_generated.py
+++ lending/patches/v1_0/rename_is_accrued_to_demand_generated.py
@@ -1,6 +1,7 @@
+import frappe
 from frappe.model.utils.rename_field import rename_field
 
 
 def execute():
-	# Rename the field and keep data intact
-	rename_field("Repayment Schedule", "is_accrued", "demand_generated")
+	if not frappe.db.has_column("Repayment Schedule", "demand_generated"):
+		rename_field("Repayment Schedule", "is_accrued", "demand_generated")
